f3495af980db195da2ed627214bcaefcbe402441
Michel Araujo <michelarrudala@gmail.com>
2023-01-29 18:36:13 -0300

    Sucessfuly created the predict.py

--- src/predict_model.py
+++ src/predict_model.py
@@ -3,8 +3,9 @@ from utils import *
 # make the dataset
 PERIOD = '800d'
 INTERVAL = '1d'
-
 STOCK_NAME = 'BOVA11.SA'  #str(input("Which stock do you want to track? "))
+
+# download the dataset
 make_dataset(STOCK_NAME, PERIOD, INTERVAL)
 
 # load the raw dataset
@@ -15,82 +16,32 @@ stock_df['Date'] = pd.to_datetime(stock_df['Date'])
 features_list = ["day_of_month", "month", "quarter", "Close_lag_1"]
 stock_df_feat = build_features(stock_df, features_list)
 
-def make_out_of_sample_predictions(X:pd.DataFrame, y:pd.Series, forecast_horizon: int) -> pd.DataFrame:
+def main():
     """
-    Make predictions for the next `forecast_horizon` days using a XGBoost model
-    
+    Main function that creates a future dataframe, makes predictions, and prints the predictions.
+
     Parameters:
-        X (pandas dataframe): The input data
-        y (pandas dataframe): The target data
-        forecast_horizon (int): Number of days to forecast
-        
+        stock_df_feat (pandas dataframe) The complete dataset
+
     Returns:
         None
     """
-
-    logger.info("Starting the pipeline..")
-
-
-    # Create empty list for storing each prediction
-    predictions = []
-    actuals = []
-    dates = []
-
-
-    # Iterate over the dataset to perform predictions over the forecast horizon, one by one.
-    # So we need to start at training = training until the total forecast horizon, then, perform the next step
-    # After forecasting the next step, we need to append the new line to the training dataset and so on
-
-    for day in range(forecast_horizon, 0, -1):
-
-        # update the training and testing sets
-        X_train = X.iloc[:-day, :]
-        y_train = y.iloc[:-day]
- 
-        if day != 1:
-            # the testing set will be the next day after the training
-            X_test = X.iloc[-day:-day+1,:]
-            y_test = y.iloc[-day:-day+1]
-
-        else:
-            # need to change the syntax for the last day (for -1:-2 will not work)
-            X_test = X.iloc[-day:,:]
-            y_test = y.iloc[-day:]
-
-
-        # only the first iteration will use the true value of y_train
-        # because the following ones will use the last predicted value as true value
-        # so we simulate the process of predicting out-of-sample
-        if len(predictions) != 0:
-            # update the y_train with the last predictions
-            y_train.iloc[-len(predictions):] = predictions[-len(predictions):]
-
-            # now update the Close_lag_1 feature
-            X_train.iloc[-len(predictions):, -1] = y_train.shift(1).iloc[-len(predictions):]
-            X_train = X_train.dropna()
-
-        else:
-            pass
-        
-        
-        # train the model
-        xgboost_model = train_model(X_train.drop("Date", axis=1), y_train)
-
-        # make prediction
-        prediction = xgboost_model.predict(X_test.drop("Date", axis=1))
-
-        # store the results
-        predictions.append(prediction[0])
-        actuals.append(y_test.values[0])
-        dates.append(X_test["Date"].max())
-
     
-    # Calculate the resulting metric
-    model_mape = round(mean_absolute_percentage_error(actuals, predictions), 4)
-    model_rmse = round(np.sqrt(mean_squared_error(actuals, predictions)), 2)
- 
-    pred_df = pd.DataFrame(list(zip(dates, actuals, predictions)), columns=["Date", 'Actual', 'Forecast'])
-    print(pred_df)
-    #visualize_validation_results(pred_df, model_mape, model_rmse)
+    # Create the future dataframe using the make_future_df function
+    future_df = make_future_df(model_config["FORECAST_HORIZON"], stock_df_feat, features_list)
+    
+    # Make predictions using the future dataframe and specified forecast horizon
+    predictions_df = make_predict(
+        forecast_horizon=model_config["FORECAST_HORIZON"]-4,
+        future_df=future_df
+    )
+    
+    # Print the predictions
+    print(predictions_df)
 
-    return pred_df
+# Execute the whole pipeline
+if __name__ == "__main__":
+
+    main()
+
+    logger.info("Pipeline was sucessful!")
