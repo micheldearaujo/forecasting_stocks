c090bb4d9b00043c977c18428cd7f553dd3c531f
michelarrudala@gmail.com <michelarrudala@gmail.com>
2023-04-27 12:32:41 +0000

    fixing the inference_model testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -398,9 +398,10 @@ def test_train_inference_model():
     X_train = pd.DataFrame({'feature1': [1, 2, 3], 'feature2': [4, 5, 6]})
     y_train = pd.Series([10, 20, 30])
     params = {'max_depth': 3, 'learning_rate': 0.1, 'n_estimators': 100}
+    stock_name = 'BOVA11.SA'
 
     # train the model
-    xgboost_model = train_inference_model(X_train, y_train, params)
+    xgboost_model = train_inference_model(X_train, y_train, params, stock_name)
 
     # check that the model is an instance of XGBRegressor
     assert isinstance(xgboost_model, xgb.sklearn.XGBRegressor)
